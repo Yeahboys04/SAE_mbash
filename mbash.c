7d4ea31e77f582104da6c4424df49faf6ae276c6
Yeahboys04 <ryankorban2004@gmail.com>
2025-01-14 09:42:43 +0100

    Premier commit

---
+++ mbash.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+// Prototypes des fonctions
+void changer_repertoire(char *chemin);
+void afficher_repertoire_courant();
+void executer_commande(char **arguments, int en_arriere_plan);
+// Fonction principale
+int main() {
+
+    printf("Bienvenu sur mbash\n");
+    return 0;
+}
